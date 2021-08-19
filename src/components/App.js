4f6e3f3a18fb095004bc2c99695756717dcde1d4
JamesCrowleyTech <jamescrowley467@gmail.com>
2021-08-19 20:46:39 +0100

    Added list of items

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Sidebar from "./sidebar";
+import Modal from "./modal";
+
+function App() {
+    return (
+        <>
+            <Sidebar></Sidebar>
+            <Modal></Modal>
+        </>
+    );
+}
+
+export default App;
---
+++ src/components/modal.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./modal.css";
+
+export default function Modal() {
+    return <div></div>;
+}
