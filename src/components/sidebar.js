d73ca657e9ac5b7976455b3702b0ff40c1ea67a1
JamesCrowleyTech <jamescrowley467@gmail.com>
2021-08-20 10:54:17 +0100

    Added sidebar open and close functionality

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,13 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import "./sidebar.css";
 import projectTitle from "../project_title.png";
 
+let SidebarContext = React.createContext();
+
 const SidebarButton = function () {
+    let { isSidebarOpen, setIsSidebarOpen } = useContext(SidebarContext);
     return (
         <button
             type="button"
             className="sidebar-open-button"
-            onClick={function () {}}
+            onClick={function () {
+                setIsSidebarOpen(!isSidebarOpen);
+            }}
         >
             <svg viewBox="0 0 448 512" xmlns="http://www.w3.org/2000/svg">
                 <path d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"></path>
@@ -36,7 +41,7 @@ const SidebarListItem = function ({ path, title }) {
 };
 
 const SidebarBody = function () {
-    let [isSidebarOpen, setIsSidebarOpen] = useState(false);
+    let { isSidebarOpen, setIsSidebarOpen } = useContext(SidebarContext);
 
     return (
         <>
@@ -44,12 +49,15 @@ const SidebarBody = function () {
                 <div className="sidebar-top-row">
                     <img className="sidebar-image" src={projectTitle} alt="" />
                     <svg
+                        className="sidebar-close-button"
                         stroke="currentColor"
                         fill="currentColor"
                         stroke-width="0"
                         viewBox="0 0 352 512"
-                        className="sidebar-close-button"
                         xmlns="http://www.w3.org/2000/svg"
+                        onClick={function () {
+                            setIsSidebarOpen(!isSidebarOpen);
+                        }}
                     >
                         <path d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"></path>
                     </svg>
@@ -82,10 +90,12 @@ const SidebarBody = function () {
 };
 
 export default function Sidebar() {
+    let [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
     return (
-        <>
+        <SidebarContext.Provider value={{ isSidebarOpen, setIsSidebarOpen }}>
             <SidebarButton />
             <SidebarBody />
-        </>
+        </SidebarContext.Provider>
     );
 }
