2a1dfaa7ebc99b82fd684643899003ccc48774de
JamesCrowleyTech <jamescrowley467@gmail.com>
2021-08-20 11:48:28 +0100

    Added model open-close functionality

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import "./modal.css";
 
+const ModalContext = React.createContext();
+
 const ModalButton = function () {
+    let { isModalOpen, setIsModalOpen } = useContext(ModalContext);
+
     return (
-        <button type="button" className="modal-open-button">
+        <button
+            type="button"
+            className="modal-open-button"
+            onClick={function () {
+                setIsModalOpen(!isModalOpen);
+            }}
+        >
             OPEN MODAL
         </button>
     );
 };
 
 const ModalBody = function () {
+    let { isModalOpen, setIsModalOpen } = useContext(ModalContext);
+
     return (
-        <div className="modal-body">
+        <div className={isModalOpen ? "modal-body modal-open" : "modal-body"}>
             <h2 className="modal-title">Modal Content</h2>
             <button
                 type="button"
@@ -25,7 +37,9 @@ const ModalBody = function () {
                     stroke-width="0"
                     viewBox="0 0 352 512"
                     xmlns="http://www.w3.org/2000/svg"
-                    onClick={function () {}}
+                    onClick={function () {
+                        setIsModalOpen(!isModalOpen);
+                    }}
                 >
                     <path d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"></path>
                 </svg>
@@ -35,10 +49,12 @@ const ModalBody = function () {
 };
 
 export default function Modal() {
+    let [isModalOpen, setIsModalOpen] = useState(false);
+
     return (
-        <div className="modal">
+        <ModalContext.Provider value={{ isModalOpen, setIsModalOpen }}>
             <ModalButton />
             <ModalBody />
-        </div>
+        </ModalContext.Provider>
     );
 }
